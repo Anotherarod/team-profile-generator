da9e7e34026e42cc9ab4ae2e8b11917d3ffbd003
Alex Aguirrebena <alex.aguirrebena@gmail.com>
2020-07-15 21:42:29 -0700

    Completed js files in libs folder

--- app.js
+++ app.js
@@ -76,4 +76,19 @@ function AppPrompt() {
             }
           });
         }
-      
+     
+        function buildTeam() {
+            // Create the output directory if the output path doesn't exist
+            if (!fs.existsSync(OUTPUT_DIR)) {
+              fs.mkdirSync(OUTPUT_DIR)
+            }
+            fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+          }
+        
+          addManager();
+        
+        }
+        
+        
+        AppPrompt();
+        
