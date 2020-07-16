81f106bead46236b4fc96bdc889777d3a1d278ff
Alex Aguirrebena <alex.aguirrebena@gmail.com>
2020-07-15 19:55:26 -0700

    Add addManager function with console log and user questions

--- app.js
+++ app.js
@@ -12,3 +12,33 @@ const render = require("./lib/htmlRenderer");
 
 const teamMembers = [];
 const idArray = [];
+
+function AppPrompt() {
+
+    function addManager() {
+      console.log("Please build your team");
+      inquirer.prompt([
+        {
+          type: "input",
+          name: "managerName",
+          message: "What is your manager's name?",
+          
+        },
+        {
+          type: "input",
+          name: "managerId",
+          message: "What is your manager's id?",
+         
+        },
+        {
+          type: "input",
+          name: "managerEmail",
+          message: "What is your manager's email?",
+          
+        },
+        {
+          type: "input",
+          name: "managerOfficeNumber",
+          message: "What is your manager's office number?",
+         
+        }
